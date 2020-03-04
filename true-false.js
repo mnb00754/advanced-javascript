fb6876c682dcf9578435783f5ba19c6b20de6591
M. NIzam <30585629+mnb754@users.noreply.github.com>
2020-03-05 03:19:04 +0600

    true vs false

--- true-false.js
+++ true-false.js
@@ -1,4 +1,14 @@
-// const name = "Solaiman";
+// it will be false
+//0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// it will be true
+// '0', ' ' , [], {}, 'false'
+
 let name = null;
 console.log(name);
 if (name) {
